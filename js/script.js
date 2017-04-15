59e484a923fc4b9318c866a9c66dc95b7a131569
Abdiwawi <abdijamzilan@gmail.com>
2017-04-16 00:57:48 +0300

    add comments to js code

--- js/script.js
+++ js/script.js
@@ -1,24 +1,25 @@
-/* Set the width of the side navigation to 250px */
+/*Sidenav script starts here*/
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
 }
 
-/* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+/*End of Sidenav script here*/
+/*Start of a new function*/
 $(document).ready(function() {
-    $("#blanks form").submit(function(event) {
+    $("#blanks form").submit(function(event) { /*referencing of the html form*/
       event.preventDefault();
     var entry1Input = $("input#entry1").val();
-    var list = $("div.output ul");
+    var list = $("div.output ul");/*a dot almost messed my code up*/
     list.empty();
     for (var i = 1; i <= entry1Input; i++) {
      list.append(`<li>${pingPongtext(i)}</li>`);
    }
   });
 });
-function pingPongtext(number){
+function pingPongtext(number){ /*Declared a function */
     switch (true) {
       case (number % 15) === 0:
         return "PingPong!";
